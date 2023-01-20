fe2ebbdd29a5ae1bc0034bb0b8d2143b832551a6
Shiviara <113165919+Shiviara@users.noreply.github.com>
2023-01-20 15:34:16 +0100

    change color on click

---
+++ places.js
@@ -0,0 +1,9 @@
+console.log("Searching for places in Aarhus...")
+
+/*function highlight() {
+    document.getElementById("red").style.backgroundColor = "red"
+}*/
+
+function highlight() {
+    document.getElementById("red").style.backgroundColor = "red"
+}
