886247c7975a54fe232ca3f30ec111e3a5d3b975
Shiviara <113165919+Shiviara@users.noreply.github.com>
2023-01-20 20:17:58 +0100

    it kinda works

--- map.js
+++ map.js
@@ -1,5 +1,5 @@
 console.log("Mapping in process")
-
+/*
 mapboxgl.accessToken = 'pk.eyJ1Ijoic2hpdmlhcmEiLCJhIjoiY2xkNG8ybWZlMGRrazN3bWRmZmQ5aHVjNSJ9.1ZI7OPI3i2MKBCteB9V1Ng';
 
 const map = new mapboxgl.Map({
@@ -8,4 +8,4 @@ const map = new mapboxgl.Map({
     style: 'mapbox://styles/shiviara/cld4n27ti001101kayi47xyx4', // style URL
     center: [10.203477, 56.168600], // starting position [lng, lat]
     zoom: 12.7 // starting zoom
-    });
+    });*/
