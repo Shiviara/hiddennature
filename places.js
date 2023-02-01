091ce1dfcf6540fbb49fbb62764703b93fd4c934
Shiviara <113165919+Shiviara@users.noreply.github.com>
2023-02-01 15:40:27 +0100

    fixes

--- places.js
+++
@@ -1,9 +0,0 @@
-console.log("Searching for places in Aarhus...")
-
-/*function highlight() {
-    document.getElementById("red").style.backgroundColor = "red"
-}*/
-
-function highlight() {
-    document.getElementById("red").style.backgroundColor = "red"
-}
